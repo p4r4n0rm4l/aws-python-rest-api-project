32dd61b88f1b35a451104cf29b93d5ce4e2dfd22
P4R4N0RM4L <l2_gold_4_ever@hotmail.com>
2021-09-13 00:17:02 +0300

    Test Commit

--- handler.py
+++ handler.py
@@ -1,10 +1,11 @@
 import json
+import modules
 
 
 def hello(event, context):
     body = {
         "message": "Go Serverless v2.0! Your function executed successfully!",
-        "input": event,
+        "input": 400,
     }
 
     response = {"statusCode": 200, "body": json.dumps(body)}
---
+++ modules/message.py
@@ -0,0 +1,11 @@
+import json
+
+def message(event):
+    body = {
+        "message": "Go Serverless v2.0! Your function executed successfully!",
+        "input": event,
+    }
+
+    response = {"statusCode": 200, "body": json.dumps(body)}
+
+    return(response)
